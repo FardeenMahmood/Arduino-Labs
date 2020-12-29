de672974654188822c35a3f7b4039d70e095f576
Fardeen Mahmood <76606341+FardeenMahmood@users.noreply.github.com>
2020-12-29 18:14:12 -0500

    Update Lab_01_LED_On_Off.ino

--- Lab_01_LED_On_Off.ino
+++ Lab_01_LED_On_Off.ino
@@ -1,14 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
+  // Setup code 
   pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // LED turns on and off repeatedly 
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH); // Turns LED on 
+  delay(1000); // 1 second delay
+  digitalWrite(LED_BUILTIN, LOW); // Turns LED off 
+  delay(1000); // 1 second delay
 }
